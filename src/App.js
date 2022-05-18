dfefd433ae6a4faf87418dd050cee7f66ada6ce1
Shivam Agarwal <shivgun03@gmail.com>
2022-05-18 13:58:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import SearchBox from "./components/search-box/search-box.component.jsx";
+import "./App.css";
+import CardList from "./components/cards-list/card-list.component.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    console.log("Constructer");
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+  render() {
+    console.log("render");
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <>
+        <div className="App">
+          <h1 className="app-title">Monsters World</h1>
+          <SearchBox onSearchChange={onSearchChange} className="search-box" placeholder = "Search Monsters" />
+          <CardList monsters={filteredMonsters} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/cards-list/card-list.component.jsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import "./card-list.styles.css";
+import Card from "../card-component/card-component.component";
+
+
+class CardList extends Component{
+    
+    render(){
+        const {monsters} = this.props;
+        return(
+            <div className='card-list'>
+               { monsters.map(monster =>{
+                    return(
+                        
+                       <Card monster = {monster}/>
+                        
+                    )
+                
+                })}
+            </div>
+        )
+    }
+}
+export default CardList
